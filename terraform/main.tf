6cad832020db7fe086222a59b091e467a5272b2b
rbkadiyala <ramesh.b.kadiyala@gmail.com>
2025-02-26 17:36:49 +0000

    ci-cd.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.3.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # Use the latest version for AWS provider
+      version = ">= 5.83"
     }
   }
-  required_version = ">= 1.5.0"  # Use the latest stable Terraform version
 }
 
 provider "aws" {
@@ -23,22 +24,29 @@ locals {
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   instance_type        = "t2.small"
-  node_desired_capacity = 1
-  node_max_capacity    = 2
-  node_min_capacity    = 1
+  node_desired_size    = 1
+  node_max_size        = 2
+  node_min_size        = 1
   environment          = "dev"
   enable_nat_gateway   = false
+  single_nat_gateway   = false
   enable_vpn_gateway   = false
   encryption_type      = "AES256"
   image_tag_mutability = "IMMUTABLE"
+  
+  tags = {
+    Environment = local.environment
+    Terraform   = "true"
+  }
 
-
+  /*
   # IAM Policies for the EKS Cluster
   eks_iam_policies = [
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ]
+  ]*/
 }
+
 
 resource "aws_ecr_repository" "my_ecr_repository" {
   name                 = local.ecr_repository_name
@@ -51,15 +59,12 @@ resource "aws_ecr_repository" "my_ecr_repository" {
     prevent_destroy = true
   }
 
-  tags = {
-    Environment = local.environment
-    Terraform   = "true"
-  }
+  tags = local.tags
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"  # Ensure to use a version that aligns with your Terraform version
+  version = "~> 5.0"  # Ensure to use a version that aligns with your Terraform version
 
   name          = local.vpc_name
   cidr          = local.cidr_block
@@ -68,47 +73,53 @@ module "vpc" {
   public_subnets  = local.public_subnets
 
   enable_nat_gateway = local.enable_nat_gateway
+  single_nat_gateway = local.single_nat_gateway  
   enable_vpn_gateway = local.enable_vpn_gateway
 
-  tags = {
-    Terraform   = "true"
-    Environment = local.environment
-  }
+  tags = local.tags
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"  # Use latest version of the EKS module
+  version = "~> 20.0"  # Use latest version of the EKS module
 
-  cluster_name = local.cluster_name
-  subnets      = module.vpc.public_subnets
-  vpc_id       = module.vpc.vpc_id
-
+  cluster_name 		= local.cluster_name
+  cluster_version 	= "1.31"
+  
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+  
+  vpc_id        = module.vpc.vpc_id
+  subnet_ids 	= module.vpc.private_subnets
+  
   tags = {
     Terraform   = "true"
     Environment = local.environment
   }
 
-  node_groups = {
-    default = {
-      instance_type    = local.instance_type
-      desired_capacity = local.node_desired_capacity
-      max_capacity     = local.node_max_capacity
-      min_capacity     = local.node_min_capacity
+  eks_managed_node_groups = {
+    one = {
+      instance_type= local.instance_type
+      max_size     = local.node_max_size
+      min_size     = local.node_min_size
+      desired_size = local.node_desired_size	  
 
-      additional_tags = {
-        Terraform   = "true"
-        Environment = local.environment
-      }
     }
   }
-
+  
+  #tags = local.tags
+  /*
   # Configure IAM role for the EKS Cluster (optional best practice)
-  cluster_iam_role_additional_policies = local.eks_iam_policies
+  cluster_iam_role_additional_policies = local.eks_iam_policies*/
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.this.repository_url
+  value = aws_ecr_repository.my_ecr_repository.repository_url
 }
 
 output "eks_cluster_name" {
