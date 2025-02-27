b80c9df2f11f7c668e2742a06f021d17d3303e3d
rbkadiyala <ramesh.b.kadiyala@gmail.com>
2025-02-27 08:34:56 +0000

    Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,133 +1,117 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"  # Ensure compatibility with Terraform version
-    }
+provider "aws" {
+  region = local.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
-  required_version = ">= 1.5.0"  # Use the latest stable Terraform version
 }
 
 locals {
-  # Define all variables in the locals block
-  aws_region           = "us-east-1"
-  cluster_name         = "my-eks-cluster"
-  ecr_repository_name  = "my-spring-boot-app"
-  vpc_name             = "my-vpc"
-  cidr_block           = "10.0.0.0/16"
-  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  instance_type        = "t2.small"
-  node_desired_size    = 1
-  node_max_size        = 2
-  node_min_size        = 1
-  environment          = "dev"
-  enable_nat_gateway   = false
-  single_nat_gateway   = false
-  enable_vpn_gateway   = false
-  encryption_type      = "AES256"
-  image_tag_mutability = "IMMUTABLE"
+  project      = "demo"
+  environment  = "dev"
+  region       = "us-east-1"
+  vpc_name     = "${local.project}-vpc-${random_integer.sequence[0].result}"
+  cluster_name = "${local.project}-eks-${random_integer.sequence[0].result}"
   
   tags = {
+    Project     = local.project
     Environment = local.environment
-    Terraform   = "true"
+    Cluster     = local.cluster_name
   }
-
-  /*
-  # IAM Policies for the EKS Cluster
-  eks_iam_policies = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ]*/
 }
 
-provider "aws" {
-  region = local.aws_region
+variable "sequence_count" {
+  description = "Number of sequence IDs to generate"
+  type        = number
+  default     = 5
 }
 
-resource "aws_ecr_repository" "my_ecr_repository" {
-  name                 = local.ecr_repository_name
-  image_tag_mutability = local.image_tag_mutability
-  encryption_configuration {
-    encryption_type = local.encryption_type
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = local.tags
+resource "random_integer" "sequence" {
+  count   = var.sequence_count
+  min     = 1
+  max     = 1000000
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"  # Ensure to use a version that aligns with your Terraform version
+  version = "5.8.1"
 
-  name          = local.vpc_name
-  cidr          = local.cidr_block
-  azs           = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
+  name = local.vpc_name
 
-  enable_nat_gateway = local.enable_nat_gateway
-  single_nat_gateway = local.single_nat_gateway  
-  enable_vpn_gateway = local.enable_vpn_gateway
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  tags = local.tags
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"  # Use latest version of the EKS module
+  version = "20.8.5"
 
-  cluster_name 		= local.cluster_name
-  cluster_version 	= "1.31"
-  
-  # EKS Addons
+  cluster_name    = local.cluster_name
+  cluster_version = "1.31"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
   cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
   }
-  
-  vpc_id        = module.vpc.vpc_id
-  subnet_ids 	= module.vpc.private_subnets
-  
-  tags = {
-    Terraform   = "true"
-    Environment = local.environment
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
   }
 
   eks_managed_node_groups = {
-    one = {
-      instance_type= local.instance_type
-      max_size     = local.node_max_size
-      min_size     = local.node_min_size
-      desired_size = local.node_desired_size	  
+    two = {
+      name = "node-group-2"
 
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
-  
-  #tags = local.tags
-  /*
-  # Configure IAM role for the EKS Cluster (optional best practice)
-  cluster_iam_role_additional_policies = local.eks_iam_policies*/
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.my_ecr_repository.repository_url
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_arn" {
-  value = module.eks.cluster_arn
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
